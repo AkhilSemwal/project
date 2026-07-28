15a9765ec308105b36087f3ec309b3ec650f6892
Priyanshugairola-9 <pgairola9945@gmail.com>
2026-07-28 22:51:01 +0530

    Implement welcome menu in main function
    Added a welcome menu function to the main program for user interaction. All CLI

--- Project_DSA/Cash_flow_minimizer/main.c
+++ Project_DSA/Cash_flow_minimizer/main.c
@@ -6,3 +6,30 @@
 #include "group.h"
 #include "expense.h"
 #include "cashflow.h"
+
+//declarations for menu
+static void welcome_menu(void);
+static void main_menu(void);
+static void group_menu(void);
+static void reports_menu(int group_index);
+
+int main(void) {
+  
+  printf("======================================================\n");
+  printf("   SMART EXPENSE TRACKER WITH CASH FLOW MINIMIZER\n");
+  printf("======================================================\n");
+  welcome_menu();
+
+  return 0;
+}
+
+//welcome menu
+static void welcome_menu(void) {
+    while (1) {
+        printf("\n===================== WELCOME =====================\n");
+        printf(" 1. Register\n");
+        printf(" 2. Login\n");
+        printf(" 3. Exit\n");
+        printf("====================================================\n");
+        printf("Enter Choice: ");
+}
